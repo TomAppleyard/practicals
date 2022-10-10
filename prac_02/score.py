2bea19c3c353d1f02d2fc09251f5786771fe42a1
TomAppleyard <thomas.appleyard@my.jcu.edu.au>
2022-10-10 14:52:17 +1000

    Processing scores using functions

--- prac_02/score.py
+++ prac_02/score.py
@@ -7,11 +7,11 @@ import random
 
 def main():
     """main function for processing scores"""
-    score = float(input("Enter score: "))
-    print(f"score: \n{return_score(score)}")
+    score = input("Enter score: ")
+    print(f"score: \n{return_result(score)}")
 
 
-def return_score(score):
+def return_result(score):
     """return result based on score provided in main"""
     if score < 0 or score > 100:
         return "invalid score"
@@ -26,4 +26,4 @@ def return_score(score):
 main()
 
 value = random.randint(0, 100)
-print(f"\nthe random number is {value} which gives a score of: \n{return_score(value)}")
+print(f"\nthe random number is {value} which gives a score of: \n{return_result(value)}")
