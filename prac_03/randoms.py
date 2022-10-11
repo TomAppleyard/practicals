bec600d102b569880abd94e3ae7399bc1c41e87d
TomAppleyard <thomas.appleyard@my.jcu.edu.au>
2022-10-11 11:53:18 +1000

    modules

---
+++ prac_03/randoms.py
@@ -0,0 +1,5 @@
+# smallest = 5,   largest = 19
+# smallest = 3,   largest = 7.            It couldn't have produced 4
+# smallest = 2.5, largest = 5.499999etc.
+import random
+print(random.randint(0, 101))
