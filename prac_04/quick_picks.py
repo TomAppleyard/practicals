8c41b9afe1ffb74b9194271ee337107aa3a33aed
TomAppleyard <thomas.appleyard@my.jcu.edu.au>
2022-10-15 15:01:13 +1000

    Quick picks exercise

--- prac_04/quick_picks.py
+++ prac_04/quick_picks.py
@@ -1,12 +1,15 @@
 import random
+NUMOF_RANDOM_NUMBERS = 6
+MINIMUM = 1
+MAXIMUM = 46
 
 numof_quick_picks = int(input("Number of quick picks: "))
 numbers = []
 
 for i in range(numof_quick_picks):
     numbers = []
-    for j in range(6):
-        number = random.randint(1, 46)
+    for j in range(NUMOF_RANDOM_NUMBERS):
+        number = random.randint(MINIMUM, MAXIMUM)
         while number in numbers:
             number = random.randint(1, 46)
         numbers.append(number)
